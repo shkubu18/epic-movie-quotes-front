1521aa4203fb977d8c850af652274a397b01fda6
data <data.shkubuliani@redberry.ge>
2023-06-21 16:44:36 +0400

    feat: add shared language switcher component

---
+++ src/components/shared/LanguageSwitcher.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center mr-9 text-white">
+    <span class="mr-2.5">Eng</span>
+    <IconArrowDown />
+  </div>
+</template>
+<script setup>
+import IconArrowDown from '@/components/icons/IconArrowDown.vue'
+</script>
