6f51d38a45a7acbed0378c5d71a7911f4d2e4429
data <data.shkubuliani@redberry.ge>
2023-06-21 17:23:07 +0400

    chore: add costum font

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -2,7 +2,9 @@
   <div class="h-screen bg-gray-950">
     <the-header />
     <div class="flex items-center justify-center flex-col h-full -mt-24">
-      <h1 class="text-6xl text-yellow text-center font-bold leading-normal mb-6">
+      <h1
+        class="text-6xl text-yellow text-center font-bold leading-normal mb-6 font-[montserrat-bold]"
+      >
         Find any quote in <br />millions of movie lines
       </h1>
       <the-button class="bg-red text-xl py-2.5">Get Started</the-button>
