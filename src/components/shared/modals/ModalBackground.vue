9a7faa52e033722b99cb95be965c7eab91745275
data <data.shkubuliani@redberry.ge>
2023-06-23 16:41:12 +0400

    feat: add shared components for modals

---
+++ src/components/shared/modals/ModalBackground.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    v-if="modals[props.modal]"
+    @click="toggleModalVisibility(props.modal)"
+    class="absolute z-10 w-full h-screen backdrop-blur-xs flex items-center justify-center"
+  ></div>
+</template>
+
+<script setup>
+import { useModalStore } from '@/stores/useModalStore'
+
+const props = defineProps({
+  modal: {
+    required: true,
+    type: String
+  }
+})
+
+const modalStore = useModalStore()
+const modals = modalStore.modals
+
+const toggleModalVisibility = (modalName) => {
+  modalStore.toggleModalVisibility(modalName)
+}
+</script>
---
+++ src/components/shared/modals/ModalContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="absolute left-1/2 right-1/2 top-1/2 bottom-1/2 flex items-center justify-center z-20">
+    <slot />
+  </div>
+</template>
