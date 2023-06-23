b692bf85f4c198d777c5312f5cfc4f69d945a0f2
data <data.shkubuliani@redberry.ge>
2023-06-23 16:52:25 +0400

    chore: import vee-validate rules and messages in main js file

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './plugins/vee-validate/rules'
+import './plugins/vee-validate/messages'
 
 const app = createApp(App)
 
