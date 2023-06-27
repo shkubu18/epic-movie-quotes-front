b127a9f23b5b5529bd7620eb4463c7da6870f9c7
data <data.shkubuliani@redberry.ge>
2023-06-27 17:16:29 +0400

    feat: add registration related modals in landing view * add spinner with background component to show when spinner is active * add click event listener to get started button

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,5 +1,11 @@
 <template>
   <login-modal v-if="modals.loginModal" />
+  <register-modal v-if="modals.registerModal" />
+  <email-sent-modal v-if="modals.emailSentModal" />
+  <verification-link-expired-modal v-if="modals.verificationLinkExpiredModal" />
+  <already-verified-email-modal v-if="modals.alreadyVerifiedEmailModal" />
+  <account-is-activated-modal v-if="modals.accountIsActivatedModal" />
+  <spinner-with-background v-if="spinnerStore.isActive" />
   <div class="h-screen bg-gray-950">
     <landing-header />
     <div class="flex items-center justify-center flex-col h-full -mt-24">
@@ -8,7 +14,9 @@
       >
         Find any quote in <br />millions of movie lines
       </h1>
-      <button-base class="bg-red text-xl py-2.5">Get Started</button-base>
+      <button-base @click="toggleModalVisibility('loginModal')" class="bg-red text-xl py-2.5">
+        Get Started
+      </button-base>
     </div>
   </div>
   <landing-quotes />
@@ -21,7 +29,19 @@ import LandingHeader from '@/components/landing/LandingHeader.vue'
 import LandingFooter from '@/components/landing/LandingFooter.vue'
 import LoginModal from '@/components/modals/LoginModal.vue'
 import { useModalStore } from '@/stores/useModalStore'
+import RegisterModal from '@/components/modals/RegisterModal.vue'
+import EmailSentModal from '@/components/modals/EmailSentModal.vue'
+import VerificationLinkExpiredModal from '@/components/modals/VerificationLinkExpiredModal.vue'
+import AlreadyVerifiedEmailModal from '@/components/modals/AlreadyVerifiedEmailModal.vue'
+import SpinnerWithBackground from '@/components/shared/SpinnerWithBackground.vue'
+import { useSpinnerStore } from '@/stores/useSpinnerStore'
+import AccountIsActivatedModal from '@/components/modals/AccountIsActivatedModal.vue'
 
 const modalStore = useModalStore()
 const modals = modalStore.modals
+const spinnerStore = useSpinnerStore()
+
+const toggleModalVisibility = (modalName) => {
+  modalStore.toggleModalVisibility(modalName)
+}
 </script>
