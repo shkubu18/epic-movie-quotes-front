ecf2ba3f9e7b82013155b085689acfa0b28100ba
data <data.shkubuliani@redberry.ge>
2023-06-27 16:54:45 +0400

    feat: add new modals for registration and also update login modal

---
+++ src/components/modals/EmailSentModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <modal-background modal="emailSentModal" />
+  <modal-container>
+    <div class="bg-dark-blue mb-12 text-white px-24 py-20 rounded-lg text-center">
+      <div class="w-440 flex items-center flex-col">
+        <icon-email-sent />
+        <h1 class="text-3xl font-medium mt-5">Thank You!</h1>
+        <p class="mt-7 mb-9">
+          Please check your email and follow the instructions to activate your account.
+        </p>
+        <a class="w-full" href="https://mail.google.com/">
+          <button-base class="bg-red w-full py-2.5">Go to my email</button-base>
+        </a>
+      </div>
+    </div>
+  </modal-container>
+</template>
+<script setup>
+import ModalBackground from '@/components/shared/modals/ModalBackground.vue'
+import ModalContainer from '@/components/shared/modals/ModalContainer.vue'
+import IconEmailSent from '@/components/icons/IconEmailSent.vue'
+import ButtonBase from '@/components/ui/ButtonBase.vue'
+</script>
--- src/components/modals/RegisterModal.vue
+++ src/components/modals/RegisterModal.vue
@@ -1,29 +1,29 @@
 <template>
-  <modal-background modal="loginModal" />
+  <modal-background modal="registerModal" />
   <modal-container>
     <div class="bg-dark-blue mb-12 text-white flex flex-col items-center px-32 py-12 rounded-lg">
-      <h1 class="text-3xl font-medium mb-2">Log in to your account</h1>
-      <p class="text-gray-600 mb-7">Welcome back! Please enter your details.</p>
-      <form-login />
+      <h1 class="text-3xl font-medium mb-2">Create an account</h1>
+      <p class="text-gray-600 mb-7">Start your journey!</p>
+      <form-register />
       <button-google-auth />
       <p class="text-gray-600 text-center mt-7">
         Already have an account?
-        <a class="text-blue-700 underline cursor-pointer" @click="handleClick">Sign up</a>
+        <a class="text-blue-700 underline cursor-pointer" @click="handleClick">Log in</a>
       </p>
     </div>
   </modal-container>
 </template>
 <script setup>
 import ModalBackground from '@/components/shared/modals/ModalBackground.vue'
-import FormLogin from '@/components/forms/FormLogin.vue'
 import ButtonGoogleAuth from '@/components/shared/ButtonGoogleAuth.vue'
 import ModalContainer from '@/components/shared/modals/ModalContainer.vue'
+import FormRegister from '@/components/forms/FormRegister.vue'
 import { useModalStore } from '@/stores/useModalStore'
 
 const modalStore = useModalStore()
 
 const handleClick = () => {
-  modalStore.toggleModalVisibility('loginModal')
   modalStore.toggleModalVisibility('registerModal')
+  modalStore.toggleModalVisibility('loginModal')
 }
 </script>
