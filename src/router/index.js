ec626483b337a0abc0703bc19cba6ec5e8238bdb
data <data.shkubuliani@redberry.ge>
2023-06-27 17:12:51 +0400

    feat: add new route for email verification

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '@/views/LandingView.vue'
+import VerifyEmailView from '@/views/VerifyEmailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'landing',
       component: LandingView
+    },
+    {
+      path: '/verify-email',
+      name: 'verify-email',
+      component: VerifyEmailView
     }
   ]
 })
