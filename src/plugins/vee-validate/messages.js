21e16c1704d1bb46b269a55274f091e245f98a18
data <data.shkubuliani@redberry.ge>
2023-06-27 17:12:19 +0400

    feat: add new messages and rules for vee-validate

--- src/plugins/vee-validate/messages.js
+++ src/plugins/vee-validate/messages.js
@@ -9,22 +9,32 @@ configure({
       ...en,
       messages: {
         required: '{field} field is required',
-        min: '{field} must have at least 0:{min} symbols'
+        min: '{field} must have at least 0:{min} symbols',
+        max: '{field} must have maximum 0:{max} symbols',
+        email: 'Please enter a valid email address',
+        confirmed: "{field} doesn't match the password"
       },
       names: {
         username_or_email: 'Username or email',
-        password: 'Password'
+        password: 'Password',
+        email: 'Email',
+        password_confirmation: 'Confirm password'
       }
     },
     ka: {
       ...ka,
       messages: {
         required: 'გთხოვთ შეიყვანოთ {field}',
-        min: '{field} უნდა შედგებოდეს მინიმუმ 0:{min} სიმბოლოსგან'
+        min: '{field} უნდა შედგებოდეს მინიმუმ 0:{min} სიმბოლოსგან',
+        max: '{field} უნდა შედგებოდეს მაქსიმუმ 0:{max} სიმბოლოსგან',
+        email: 'გთხოვთ შეიყვანოთ ვალიდური ელ.ფოსტის მისამართი',
+        confirmed: '{field} არ ემთხვევა პაროლს'
       },
       names: {
         username_or_email: 'მომხმარებლის სახელი ან ელ.ფოსტა',
-        password: 'პაროლი'
+        password: 'პაროლი',
+        email: 'ელ.ფოსტა',
+        password_confirmation: 'განმეორებითი პაროლი'
       }
     }
   })
--- src/plugins/vee-validate/rules.js
+++ src/plugins/vee-validate/rules.js
@@ -1,7 +1,27 @@
 import { configure, defineRule } from 'vee-validate'
-import { min, required } from '@vee-validate/rules'
+import { confirmed, email, min, required } from '@vee-validate/rules'
 
 configure({ validateOnInput: true })
 
 defineRule('required', required)
 defineRule('min', min)
+defineRule('email', email)
+defineRule('confirmed', confirmed)
+
+const lowercaseRegex = /^[a-z0-9]+$/
+
+defineRule('min-3-max-15-lowercase', (value) => {
+  if (value.length < 3 || value.length > 15 || !lowercaseRegex.test(value)) {
+    return 'At least 3 & max. 15 lowercase characters.'
+  }
+
+  return true
+})
+
+defineRule('min-8-max-15-lowercase', (value) => {
+  if (value.length < 8 || value.length > 15 || !lowercaseRegex.test(value)) {
+    return 'At least 8 & max. 15 lowercase characters.'
+  }
+
+  return true
+})
