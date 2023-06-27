cab61fa7d77dad0be036b9ecda4d1613f90465cc
data <data.shkubuliani@redberry.ge>
2023-06-27 16:57:20 +0400

    feat: add new shared component

---
+++ src/components/shared/SpinnerWithBackground.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="absolute top-0 z-20 w-full h-screen bg-[#222030e6] flex items-center justify-center flex-col"
+  >
+    <icon-loading-spinner />
+    <h1 class="text-xl text-white font-bold mt-2">Please wait...</h1>
+  </div>
+</template>
+<script setup>
+import IconLoadingSpinner from '@/components/icons/IconLoadingSpinner.vue'
+</script>
