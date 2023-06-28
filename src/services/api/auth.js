97fbcf2a8bf08258670d89254269b9e2573c020f
data <data.shkubuliani@redberry.ge>
2023-06-28 15:50:03 +0400

    refactor: move function in oauth js file

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -34,12 +34,6 @@ export async function resendEmailForVerification(email) {
   })
 }
 
-export async function authWithGoogle() {
-  return axios.get('/sanctum/csrf-cookie').then(() => {
-    location.href = import.meta.env.VITE_API_BASE_URL + '/oauth/google/redirect'
-  })
-}
-
 export async function sendPasswordForgotEmail(email) {
   return axios.post('api/password/email', {
     email: email
---
+++ src/services/api/oauth.js
@@ -0,0 +1,7 @@
+import axios from '@/plugins/axios/axios'
+
+export async function authWithGoogle() {
+  return axios.get('/sanctum/csrf-cookie').then(() => {
+    location.href = import.meta.env.VITE_API_BASE_URL + '/oauth/google/redirect'
+  })
+}
