629e9043d5da0495beae0c51b4d71ee2fc845f77
data <data.shkubuliani@redberry.ge>
2023-06-28 15:22:21 +0400

    feat: add new password forgot and update form components

---
+++ src/components/forms/FormPasswordForgot.vue
@@ -0,0 +1,51 @@
+<template>
+  <ValidationForm v-slot="{ errors }" class="w-full text-left" @submit="handleSubmit">
+    <input-auth
+      label="Email"
+      name="email"
+      placeholder="Enter your email"
+      type="text"
+      v-model="email"
+      rules="required|email"
+      :error="errors.email"
+    />
+    <span v-if="errorMessage" class="text-red mb-5 block">{{ errorMessage }}</span>
+    <button-base class="bg-red w-full py-2">Send instructions</button-base>
+  </ValidationForm>
+</template>
+<script setup>
+import { Form as ValidationForm } from 'vee-validate'
+import InputAuth from '@/components/ui/InputAuth.vue'
+import { ref } from 'vue'
+import { sendPasswordForgotEmail, setCookies } from '@/services/api/auth'
+import { useModalStore } from '@/stores/useModalStore'
+import { useSpinnerStore } from '@/stores/useSpinnerStore'
+import ButtonBase from '@/components/ui/ButtonBase.vue'
+
+const modalStore = useModalStore()
+const spinnerStore = useSpinnerStore()
+
+const toggleModalVisibility = (modalName) => {
+  modalStore.toggleModalVisibility(modalName)
+}
+
+const email = ref('')
+const errorMessage = ref('')
+
+async function handleSubmit() {
+  spinnerStore.toggleActiveStatus()
+  await setCookies()
+  await sendPasswordForgotEmail(email.value)
+    .then((response) => {
+      if (response.status === 200) {
+        toggleModalVisibility('passwordForgotModal')
+        toggleModalVisibility('passwordResetEmailSentModal')
+      }
+    })
+    .catch((error) => {
+      errorMessage.value = ''
+      errorMessage.value = error.response.data.message
+    })
+  spinnerStore.toggleActiveStatus()
+}
+</script>
---
+++ src/components/forms/FormPasswordUpdate.vue
@@ -0,0 +1,67 @@
+<template>
+  <ValidationForm v-slot="{ errors }" class="w-full text-left" @submit="handleSubmit">
+    <input-auth
+      label="Password"
+      name="password"
+      placeholder="At least 8 & max.15 lower case characters"
+      type="password"
+      v-model="password"
+      rules="min-8-max-15-lowercase"
+      :error="errors.password"
+    />
+    <input-auth
+      label="Confirm password"
+      name="password_confirmation"
+      placeholder="Confirm password"
+      type="password"
+      v-model="passwordConfirmation"
+      rules="required|confirmed:@password"
+      :error="errors.password_confirmation"
+    />
+    <button-base class="bg-red w-full py-2">Reset password</button-base>
+  </ValidationForm>
+</template>
+<script setup>
+import { Form as ValidationForm } from 'vee-validate'
+import InputAuth from '@/components/ui/InputAuth.vue'
+import { ref } from 'vue'
+import { passwordUpdate, setCookies } from '@/services/api/auth'
+import { useModalStore } from '@/stores/useModalStore'
+import { useSpinnerStore } from '@/stores/useSpinnerStore'
+import ButtonBase from '@/components/ui/ButtonBase.vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+const modalStore = useModalStore()
+const spinnerStore = useSpinnerStore()
+
+const toggleModalVisibility = (modalName) => {
+  modalStore.toggleModalVisibility(modalName)
+}
+
+const password = ref('')
+const passwordConfirmation = ref('')
+
+async function handleSubmit() {
+  spinnerStore.toggleActiveStatus()
+  await setCookies()
+  await passwordUpdate(password.value, passwordConfirmation.value, route.query.token)
+    .then((response) => {
+      if (response.status === 201) {
+        router.replace('/')
+        toggleModalVisibility('passwordUpdateModal')
+        toggleModalVisibility('passwordUpdatedSuccessfullyModal')
+      }
+    })
+    .catch((error) => {
+      if (error.response.status === 403) {
+        router.replace('/')
+        toggleModalVisibility('passwordUpdateModal')
+        toggleModalVisibility('passwordResetTokenExpiredModal')
+      }
+    })
+  spinnerStore.toggleActiveStatus()
+}
+</script>
