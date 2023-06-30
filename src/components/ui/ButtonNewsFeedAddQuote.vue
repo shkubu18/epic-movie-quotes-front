f9e7c5bbc3803fe5e22c8b25fe1f02884046a89c
data <data.shkubuliani@redberry.ge>
2023-07-01 01:30:16 +0400

    feat: add new ui components

---
+++ src/components/ui/ButtonNewsFeedAddQuote.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    :class="{
+      'flex text-white py-3 px-4 rounded-xl bg-dark-blue items-center cursor-pointer': true,
+      'w-full': !isSearchBarOpen,
+      'w-60 justify-between': isSearchBarOpen
+    }"
+  >
+    <icon-write-new-quote />
+    <span class="text-xl ml-5">Write new quote</span>
+  </div>
+</template>
+<script setup>
+import IconWriteNewQuote from '@/components/icons/IconWriteNewQuote.vue'
+
+defineProps({
+  isSearchBarOpen: {
+    required: true,
+    type: Boolean
+  }
+})
+</script>
