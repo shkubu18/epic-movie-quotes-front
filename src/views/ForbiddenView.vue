bce1e09fbab81b9f848b6d1fb214a1be72c4ebba
data <data.shkubuliani@redberry.ge>
2023-07-01 01:37:16 +0400

    feat: add notfound and forbidden views

---
+++ src/views/ForbiddenView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main
+    class="min-h-screen bg-darker-blue flex items-center justify-center flex-col text-white relative"
+  >
+    <icon-forbidden class="absolute top-48 z-10" />
+    <icon-forbidden-circles class="absolute top-60" />
+    <h1 class="text-5xl font-bold">You shall not pass!</h1>
+    <p class="text-2xl mt-5 mb-14">Sorry, but you don’t have permission to access this page</p>
+    <button-return-home />
+  </main>
+</template>
+<script setup>
+import IconForbidden from '@/components/icons/IconForbidden.vue'
+import IconForbiddenCircles from '@/components/icons/IconForbiddenCircles.vue'
+import ButtonReturnHome from '@/components/ui/ButtonReturnHome.vue'
+</script>
