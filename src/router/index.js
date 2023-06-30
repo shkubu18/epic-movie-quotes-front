3a0a4b4ff5240cf473181652d1df9f6c43ed282d
data <data.shkubuliani@redberry.ge>
2023-07-01 01:38:07 +0400

    feat: add route guards and new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '@/views/LandingView.vue'
 import VerifyEmailView from '@/views/VerifyEmailView.vue'
+import NewsFeedView from '@/views/NewsFeedView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import ForbiddenView from '@/views/ForbiddenView.vue'
+import { isAuthenticated, isNotAuthenticated } from '@/router/guard'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +12,30 @@ const router = createRouter({
     {
       path: '/',
       name: 'landing',
-      component: LandingView
+      component: LandingView,
+      beforeEnter: isNotAuthenticated
     },
     {
       path: '/verify-email',
       name: 'verify-email',
-      component: VerifyEmailView
+      component: VerifyEmailView,
+      beforeEnter: isNotAuthenticated
+    },
+    {
+      path: '/newsfeed',
+      name: 'newsfeed',
+      component: NewsFeedView,
+      beforeEnter: isAuthenticated
+    },
+    {
+      path: '/:notFound(.*)',
+      name: '404',
+      component: NotFoundView
+    },
+    {
+      path: '/403',
+      name: '403',
+      component: ForbiddenView
     }
   ]
 })
