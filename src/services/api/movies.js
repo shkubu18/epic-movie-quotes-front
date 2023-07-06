a556421955b719528cc1b3b655a718524e9d05e5
data <data.shkubuliani@redberry.ge>
2023-07-06 16:41:18 +0400

    feat: add new api services for movies

--- src/services/api/movies.js
+++ src/services/api/movies.js
@@ -3,3 +3,47 @@ import axios from '@/plugins/axios/axios'
 export async function searchMovies(searchText) {
   return axios.get(`/api/movies/search?search=${searchText}`)
 }
+
+export async function getUserMovies() {
+  return axios.get('/api/movies')
+}
+
+export async function getMovieGenres() {
+  return axios.get('/api/genres')
+}
+
+export async function addMovie(movieData) {
+  return axios.post('/api/movies', {
+    name_en: movieData.name_en,
+    name_ka: movieData.name_ka,
+    genres: movieData.genres,
+    release_date: movieData.release_date,
+    director_en: movieData.director_en,
+    director_ka: movieData.director_ka,
+    description_en: movieData.description_en,
+    description_ka: movieData.description_ka,
+    picture: movieData.picture
+  })
+}
+
+export async function updateMovie(movieData, movieId) {
+  return axios.post(`/api/movies/${movieId}`, {
+    name_en: movieData.name_en,
+    name_ka: movieData.name_ka,
+    genres: movieData.genres,
+    release_date: movieData.release_date,
+    director_en: movieData.director_en,
+    director_ka: movieData.director_ka,
+    description_en: movieData.description_en,
+    description_ka: movieData.description_ka,
+    picture: movieData.picture
+  })
+}
+
+export async function getMovie(movieId) {
+  return axios.get(`/api/movies/${movieId}`)
+}
+
+export async function destroyMovie(movieId) {
+  return axios.delete(`/api/movies/${movieId}`)
+}
