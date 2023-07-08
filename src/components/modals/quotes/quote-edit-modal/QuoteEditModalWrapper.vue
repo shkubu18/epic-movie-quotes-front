ce3af8d89a8849b395a8509f66b584d082397407
data <data.shkubuliani@redberry.ge>
2023-07-08 15:12:23 +0400

    feat: add quotes related modals with their components

---
+++ src/components/modals/quotes/quote-edit-modal/QuoteEditModalWrapper.vue
@@ -0,0 +1,72 @@
+<template>
+  <modal-crud-wrapper>
+    <div class="bg-lighter-black w-fit text-white ml-28 pb-7 rounded-lg">
+      <header
+        class="flex justify-center items-center w-full relative py-7 border-b-3 border-b-neutral-800"
+      >
+        <div
+          @click="deleteQuote(quoteId)"
+          class="rounded-xl text-gray-200 flex absolute top-9 left-9 cursor-pointer"
+        >
+          <icon-delete />
+          <span class="ml-3">Delete</span>
+        </div>
+        <h1 class="text-2xl font-medium">Edit Quote</h1>
+        <icon-modal-close
+          @click="toggleModalVisibility('quoteEditModal')"
+          class="absolute right-10 top-10 cursor-pointer"
+        />
+      </header>
+      <modal-crud-user-info :api-url-for-pictures="apiUrlForPictures" :user="user" />
+      <slot />
+    </div>
+  </modal-crud-wrapper>
+</template>
+
+<script setup>
+import IconModalClose from '@/components/icons/IconModalClose.vue'
+import ModalCrudWrapper from '@/components/layouts/modals/ModalCrudWrapper.vue'
+import ModalCrudUserInfo from '@/components/shared/modals/ModalCrudUserInfo.vue'
+import { useModalStore } from '@/stores/useModalStore'
+import { useUserStore } from '@/stores/useUserStore'
+import { storeToRefs } from 'pinia'
+import IconDelete from '@/components/icons/IconDelete.vue'
+import { destroyQuote } from '@/services/api/quotes'
+import { useMovieStore } from '@/stores/useMovieStore'
+import { useNewsFeedQuoteStore } from '@/stores/useNewsFeedQuoteStore'
+
+defineProps({
+  apiUrlForPictures: {
+    required: true,
+    type: String
+  },
+  quoteId: {
+    required: true,
+    type: Number
+  }
+})
+
+const modalStore = useModalStore()
+
+const userStore = useUserStore()
+const { user } = storeToRefs(userStore)
+
+const movieStore = useMovieStore()
+const { isMovieUpdated } = storeToRefs(movieStore)
+
+const newsFeedQuoteStore = useNewsFeedQuoteStore()
+
+const toggleModalVisibility = (modalName) => {
+  modalStore.toggleModalVisibility(modalName)
+}
+
+const deleteQuote = async (quoteId) => {
+  await destroyQuote(quoteId).then((response) => {
+    if (response.status === 200) {
+      modalStore.toggleModalVisibility('quoteEditModal')
+      isMovieUpdated.value = true
+      newsFeedQuoteStore.resetStore()
+    }
+  })
+}
+</script>
---
+++ src/components/modals/quotes/quote-view-modal/QuoteViewModalArticle.vue
@@ -0,0 +1,87 @@
+<template>
+  <article class="text-white bg-black w-940 rounded-xl pb-7 px-7">
+    <section class="mb-8">
+      <div
+        class="flex items-center justify-between border-2 border-crud-input-gray rounded p-2.5 mb-5"
+      >
+        <p class="italic text-2xl">{{ '"' + quote.name.en + '"' }}</p>
+        <span class="text-xl text-crud-input-gray">Eng</span>
+      </div>
+      <div class="flex items-center justify-between border-2 border-crud-input-gray rounded p-2.5">
+        <p class="italic text-2xl">{{ '"' + quote.name.ka + '"' }}</p>
+        <span class="text-xl text-crud-input-gray">ქარ</span>
+      </div>
+    </section>
+    <section>
+      <img
+        class="w-890 h-500 rounded-xl object-cover"
+        :src="apiUrlForPictures + quote.picture"
+        alt="quote picture"
+      />
+      <div class="text-left mt-6 text-xl flex pb-7 border-b-2 border-default-border-b-color">
+        <div class="flex items-center mr-8">
+          <span class="mr-4">{{ quote.total_comments }}</span>
+          <icon-comment />
+        </div>
+        <div class="flex items-center">
+          <span class="mr-4">{{ quote.total_likes }}</span>
+          <icon-like />
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="max-h-96 overflow-y-scroll">
+        <div v-for="comment in quote.comments" :key="comment.id" class="mt-5 flex text-xl">
+          <img
+            :src="
+              comment.user.profile_picture
+                ? apiUrlForPictures + comment.user.profile_picture
+                : 'https://www.citypng.com/public/uploads/preview/png-round-blue-contact-user-profile-icon-11639786938sxvzj5ogua.png'
+            "
+            alt="comment author profile picture"
+            class="h-14 w-14 rounded-full mr-6"
+          />
+          <div class="text-left border-b-2 pb-7 w-full border-default-border-b-color">
+            <h2 class="mb-5 pt-5">{{ comment.user.username }}</h2>
+            <p>{{ comment.body }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="mt-7 flex">
+        <img
+          :src="
+            user.profile_picture
+              ? apiUrlForPictures + user.profile_picture
+              : 'https://www.citypng.com/public/uploads/preview/png-round-blue-contact-user-profile-icon-11639786938sxvzj5ogua.png'
+          "
+          alt="profile picture"
+          class="h-14 rounded-full mr-6"
+        />
+        <textarea
+          class="h-14 placeholder:text-gray-300 pl-7 pt-4 bg-dark-blue border-none rounded-xl w-full"
+          name="comment"
+          placeholder="White a comment"
+        ></textarea>
+      </div>
+    </footer>
+  </article>
+</template>
+<script setup>
+import IconLike from '@/components/icons/IconLike.vue'
+import IconComment from '@/components/icons/IconComment.vue'
+
+defineProps({
+  apiUrlForPictures: {
+    required: true,
+    type: String
+  },
+  quote: {
+    required: true,
+    type: Object
+  },
+  user: {
+    required: true,
+    type: Object
+  }
+})
+</script>
