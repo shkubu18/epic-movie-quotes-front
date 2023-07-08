1bf4aab78706aeef43207324e0f00b7eeefa5af6
data <data.shkubuliani@redberry.ge>
2023-07-08 15:28:11 +0400

    chore: add props to icon component in language switcher shared component

--- src/components/shared/LanguageSwitcher.vue
+++ src/components/shared/LanguageSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center mr-9 text-white">
     <span class="mr-2.5">Eng</span>
-    <IconArrowDown />
+    <IconArrowDown width="12" height="8" />
   </div>
 </template>
 <script setup>
