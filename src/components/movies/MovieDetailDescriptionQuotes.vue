137e41fd258552e00ca8635c290db906c1b6f121
data <data.shkubuliani@redberry.ge>
2023-07-08 15:45:02 +0400

    feat: add movie description quotes menu releated funcioanlities

--- src/components/movies/MovieDetailDescriptionQuotes.vue
+++ src/components/movies/MovieDetailDescriptionQuotes.vue
@@ -2,11 +2,19 @@
   <section class="mt-20">
     <div
       class="bg-lighter-black rounded-lg w-7/12 py-6 px-6 relative mb-10"
-      v-for="quote in quotes"
+      v-for="(quote, index) in quotes"
       :key="quote.id"
     >
-      <movie-detail-description-quotes-menu :is-quote-menu-open="isQuoteMenuOpen" />
-      <icon-three-dots @click="toggleQuoteMenu" class="absolute top-5 right-6 cursor-pointer" />
+      <movie-detail-description-quotes-menu
+        :quote-menu-state="quoteMenuState"
+        :quote-id="quote.id"
+        :quote-index="index"
+        @get-quote-index="getQuoteIndex"
+      />
+      <icon-three-dots
+        @click="toggleQuoteMenu(quote.id)"
+        class="absolute top-5 right-6 cursor-pointer"
+      />
       <div class="flex items-center border-b-2 border-default-border-b-color pb-5">
         <img
           class="h-36 w-2/6 rounded-lg object-cover"
@@ -46,9 +54,16 @@ defineProps({
   }
 })
 
-const isQuoteMenuOpen = ref(false)
+const emits = defineEmits(['getQuote'])
 
-const toggleQuoteMenu = () => {
-  isQuoteMenuOpen.value = !isQuoteMenuOpen.value
+const quoteMenuState = ref([])
+
+const toggleQuoteMenu = (quoteId) => {
+  quoteMenuState.value[quoteId] = !quoteMenuState.value[quoteId]
+}
+
+const getQuoteIndex = (quoteIndex, quoteId, action) => {
+  quoteMenuState.value[quoteId] = !quoteMenuState.value[quoteId]
+  emits('getQuote', quoteIndex, action)
 }
 </script>
