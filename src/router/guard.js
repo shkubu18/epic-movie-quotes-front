c9eb4b6dd26330d65685e60bf3d661ff9560bca5
data <data.shkubuliani@redberry.ge>
2023-07-08 15:32:21 +0400

    feat: update router guards logic and also update their names

--- src/router/guard.js
+++ src/router/guard.js
@@ -1,31 +1,47 @@
 import { checkAuthStatus } from '@/services/api/auth'
+import { useUserStore } from '@/stores/useUserStore'
+import { storeToRefs } from 'pinia'
 
-export const isAuthenticated = (to, from, next) => {
-  checkAuthStatus()
-    .then((response) => {
-      if (response.status === 200) {
-        next()
-      }
-    })
-    .catch((error) => {
-      if (error.response.status === 401) {
-        console.clear()
-        next({ name: '403' })
-      }
-    })
+export const shouldAuthenticated = (to, from, next) => {
+  const userStore = useUserStore()
+  const { isUserAlreadyFetched } = storeToRefs(userStore)
+
+  if (isUserAlreadyFetched.value) {
+    next()
+  } else {
+    checkAuthStatus()
+      .then((response) => {
+        if (response.status === 200) {
+          next()
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          console.clear()
+          next({ name: '403' })
+        }
+      })
+  }
 }
 
-export const isNotAuthenticated = (to, from, next) => {
-  checkAuthStatus()
-    .then((response) => {
-      if (response.status === 200) {
-        next({ name: 'newsfeed' })
-      }
-    })
-    .catch((error) => {
-      if (error.response.status === 401) {
-        console.clear()
-        next()
-      }
-    })
+export const shouldNotAuthenticated = (to, from, next) => {
+  const userStore = useUserStore()
+  const { isUserAlreadyFetched } = storeToRefs(userStore)
+
+  if (isUserAlreadyFetched.value) {
+    next()
+  } else {
+    checkAuthStatus()
+      .then((response) => {
+        if (response.status === 200) {
+          next({ name: 'newsfeed' })
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          console.clear()
+          next()
+        }
+      })
+  }
 }
