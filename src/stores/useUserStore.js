6e5f8cb69a663aa8d01c7bc0d3a05b90ad922e22
data <data.shkubuliani@redberry.ge>
2023-07-08 15:42:13 +0400

    feat: add new state and reset function in user store

--- src/stores/useUserStore.js
+++ src/stores/useUserStore.js
@@ -3,13 +3,21 @@ import { ref } from 'vue'
 
 export const useUserStore = defineStore('useUserStore', () => {
   const user = ref({})
+  const isUserAlreadyFetched = ref(false)
 
   function addUser(userInfo) {
     user.value = userInfo
   }
 
+  function resetStore() {
+    user.value = {}
+    isUserAlreadyFetched.value = false
+  }
+
   return {
     user,
-    addUser
+    addUser,
+    isUserAlreadyFetched,
+    resetStore
   }
 })
