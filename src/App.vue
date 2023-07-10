b27f529948dc5b8c165ae96f87d63f45d96a5473
data <data.shkubuliani@redberry.ge>
2023-07-11 02:10:20 +0400

    feat: add notifications private channel listener * add delete like notification function in like removed channel

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,17 @@ import { useUserStore } from '@/stores/useUserStore'
 import { storeToRefs } from 'pinia'
 import { onMounted } from 'vue'
 import instantiatePusher from '@/helpers/instantiatePusher'
+import { getUser } from '@/services/api/auth'
+import { deleteLikeNotification } from '@/services/api/notifications'
 
 const newsFeedQuoteStore = useNewsFeedQuoteStore()
 
 const userStore = useUserStore()
 const { user } = storeToRefs(userStore)
+const { notifications } = storeToRefs(userStore)
+const { isUserAlreadyFetched } = storeToRefs(userStore)
+const { unreadNotificationsCount } = storeToRefs(userStore)
+const { isNotificationsAlreadyFetched } = storeToRefs(userStore)
 
 onMounted(async () => {
   instantiatePusher()
@@ -41,7 +47,24 @@ onMounted(async () => {
   window.Echo.channel('likes').listen('Likes\\LikeRemoved', (data) => {
     if (data.sender !== user.value.username) {
       newsFeedQuoteStore.removeLike(data.quote_id)
+      deleteLikeNotification(data.notification_id)
+      isNotificationsAlreadyFetched.value = false
     }
+  })
+
+  if (!isUserAlreadyFetched.value) {
+    await getUser()
+      .then((response) => {
+        userStore.addUser(response.data.user)
+      })
+      .finally(() => {
+        isUserAlreadyFetched.value = true
+      })
+  }
+
+  window.Echo.private(`notifications.${user.value.id}`).listen('NotificationAdded', (data) => {
+    unreadNotificationsCount.value++
+    notifications.value.unshift(data)
   })
 })
 </script>
