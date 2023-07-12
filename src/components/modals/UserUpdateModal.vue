14b255aeb0ce2a6d1aae447104cbed21c53a13a6
data <data.shkubuliani@redberry.ge>
2023-07-12 18:30:06 +0400

    feat: add user profile releated components

---
+++ src/components/modals/UserUpdateModal.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="absolute top-28 right-16 bg-light-green p-4 rounded flex items-center">
+    <icon-updated-successfully />
+    <h2 class="text-xl text-dark-green ml-3">Changes updated successfully</h2>
+  </div>
+</template>
+<script setup>
+import IconUpdatedSuccessfully from '@/components/icons/IconUpdatedSuccessfully.vue'
+</script>
