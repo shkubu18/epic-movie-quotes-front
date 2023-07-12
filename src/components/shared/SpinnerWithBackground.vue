b8bda1637ecc13891cf75401996eb776520b3ab7
data <data.shkubuliani@redberry.ge>
2023-07-12 18:51:09 +0400

    feat: add dynamic height style to shared spinner with background component

--- src/components/shared/SpinnerWithBackground.vue
+++ src/components/shared/SpinnerWithBackground.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="absolute top-0 z-20 w-full h-screen bg-[#222030e6] flex items-center justify-center flex-col"
+    class="absolute top-0 z-20 w-full bg-transparent-dark-blue flex items-center justify-center flex-col"
+    :style="`height: ${minHeight}px`"
   >
     <icon-loading-spinner />
     <h1 class="text-xl text-white font-bold mt-2">Please wait...</h1>
@@ -8,4 +9,9 @@
 </template>
 <script setup>
 import IconLoadingSpinner from '@/components/icons/IconLoadingSpinner.vue'
+import { computed } from 'vue'
+
+const minHeight = computed(() => {
+  return document.body.offsetHeight
+})
 </script>
