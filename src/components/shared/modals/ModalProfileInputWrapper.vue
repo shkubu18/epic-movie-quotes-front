81e0aec3c89056493289f140fb00fd201e15d8e6
data <data.shkubuliani@redberry.ge>
2023-07-16 18:53:00 +0400

    feat: add new shared component for mobile profile modal inputs

---
+++ src/components/shared/modals/ModalProfileInputWrapper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    v-show="!modals.mobileProfileAskConfirmationModal"
+    class="lg:hidden absolute top-36 z-20 w-full min-h-screen bg-darker-blue text-white"
+    :style="`height: ${minHeight}px`"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { useModalStore } from '@/stores/useModalStore'
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
+
+const modalStore = useModalStore()
+const { modals } = storeToRefs(modalStore)
+
+const minHeight = computed(() => {
+  return document.body.offsetHeight - 114
+})
+</script>
